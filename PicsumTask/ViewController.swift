7c3ce907f8395645f407e7ecc397c5d9b17b5c6a
Viveka <121420247+vivekarathna7@users.noreply.github.com>
2023-05-07 12:50:19 +0530

    Initial commit

---
+++ PicsumTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicsumTask
+//
+//  Created by Viveka G on 07/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
